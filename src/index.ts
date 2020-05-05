00c62a3b976c15f9058d2537a7b1b24250e530de
speanut <speanutcash@gmail.com>
2020-05-05 21:27:13 +0800

    upd: separate desgin pattern

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-export class Hero {
-  id: number;
-  name: string;
-
-  constructor(name) {
-    this.name = name;
-  }
-
-  myName() {
-    return this.name;
-  }
-}
-
-let hero = new Hero('asdasd');
-console.log(hero.myName());
+// this is placeholder
